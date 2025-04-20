5433174986696a922063f61910448c2d9b823d3f
Nirmeetchoudhary <154678798+Nirmeetchoudhary@users.noreply.github.com>
2025-04-20 19:49:38 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS0Y56xVN0HyXdsSTCwIyIoXEQpboHRUc",
+  authDomain: "foodrecipe-bdd97.firebaseapp.com",
+  projectId: "foodrecipe-bdd97",
+  storageBucket: "foodrecipe-bdd97.appspot.com",
+  messagingSenderId: "500578925809",
+  appId: "1:500578925809:web:fb87f96565758982e2fd50"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
